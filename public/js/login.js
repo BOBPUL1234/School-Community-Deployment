70cb1c18d2dc230b6e51c604992682f31c18ed7b
BOBPUL1234 <enwjdlcj@gmail.com>
2025-06-26 20:51:40 +0900

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -46,7 +46,7 @@ function registerStudent() {
         return;
     }
 
-    fetch("${BASE_URL}/auth/signup/student", {
+    fetch(`${BASE_URL}/auth/signup/student`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ id, name, password })
@@ -72,7 +72,7 @@ function registerTeacher() {
         return;
     }
 
-    fetch("${BASE_URL}/auth/signup/teacher", {
+    fetch(`${BASE_URL}/auth/signup/teacher`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, password, securityKey })
@@ -98,7 +98,7 @@ function loginStudent() {
         return;
     }
 
-    fetch("${BASE_URL}/auth/login/student", {
+    fetch(`${BASE_URL}/auth/login/student`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
@@ -133,7 +133,7 @@ function loginTeacher() {
         return;
     }
 
-    fetch("${BASE_URL}/auth/login/teacher", {
+    fetch(`${BASE_URL}/auth/login/teacher`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
